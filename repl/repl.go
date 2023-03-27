b6ca753385b99e83562c282046ba51c098769f3d
Suryansh Chauhan <suryanshchauhan2005@gmail.com>
2023-03-27 11:21:03 +0530

    feat: Read-Lex-Print-Loop -> Read-Parse-Print-Loop

--- repl/repl.go
+++ repl/repl.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/Suryansh-23/amrit/token"
-
 	"github.com/Suryansh-23/amrit/lexer"
+	"github.com/Suryansh-23/amrit/parser"
 )
 
 const PROMPT = ">>> "
@@ -22,11 +21,23 @@ func Start(in io.Reader, out io.Writer) {
 		if !scanned {
 			return
 		}
-		line := scanner.Text()
 
+		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
